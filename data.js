48baa69d432ae42c6c039b993e9bc0d324dcfee0
Hemang Kumar <hemangsk@gmail.com>
2016-10-14 20:53:57 +0530

    student dashboard done

---
+++ data.js
@@ -0,0 +1,82 @@
+var student_attributes = [
+    
+    "Attribute 1",
+    "Attribute 2",
+    "Attribute 3",
+    "Attribute 4",
+    "Attribute 5",
+    "Attribute 6",
+    "Attribute 7",
+    "Attribute 8",
+    "Attribute 9",
+    "Attribute 10",
+    "Attribute 11",
+    "Attribute 12",
+    "Attribute 13",
+    "Attribute 14",
+    "Attribute 15"
+
+];
+
+var marks = {  
+ "How are you" :  [
+    
+     {
+         "teacher_name":"T1",
+         "marks": "5"
+     },
+     
+     {
+         "teacher_name":"R2",
+         "marks": "5"
+     },
+     
+     {
+         "teacher_name":"T3",
+         "marks": "5"
+     }
+     
+ ]
+    
+};
+
+var teachers = {
+    
+    "theory" : [
+        {
+            "name" : "teacher1",
+            "image" : "http://placehold.it/120x150"
+        },
+        
+        {
+            "name" : "teacher2",
+            "image": "http://placehold.it/120x150"
+        },
+        
+        {
+            "name": "teacher3",
+            "image": "http://placehold.it/120x150"
+        },
+         {
+            "name" : "teacher1",
+            "image" : "http://placehold.it/120x150"
+        },
+        
+        {
+            "name" : "teacher2",
+            "image": "http://placehold.it/120x150"
+        },
+        
+        {
+            "name": "teacher3",
+            "image": "http://placehold.it/120x150"
+        }
+    ],
+    
+    "practical" :[
+        {
+            "name": "t4",
+            "image": "http://placehold.it/150x150"
+        }
+    ]
+}; 
